07d04ba6e1984a2e1d73f1416c0140a233926cbb
washizawa83 <washizawa83@gmail.com>
2025-01-01 01:58:54 +0900

    カレンダーコンポーネント作成

---
+++ src/app/utils/calendar/days-util.ts
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+
+export const Months = {
+    January: 0,
+    February: 1,
+    March: 2,
+    April: 3,
+    May: 4,
+    June: 5,
+    July: 6,
+    August: 7,
+    September: 8,
+    October: 9,
+    November: 10,
+    December: 11,
+};
+
+export const getMonthDays = (day: dayjs.Dayjs = dayjs()) => {
+  const year = day.year();
+  const firstDayOfTheMonth = dayjs(new Date(year, day.month(), 1)).day();
+  let currentMonthCount = 0 - firstDayOfTheMonth;
+  const daysMatrix = new Array(5).fill([]).map(() => {
+    return new Array(7).fill(null).map(() => {
+      currentMonthCount++;
+      return dayjs(new Date(year, day.month(), currentMonthCount));
+    });
+  });
+  return daysMatrix;
+}
+
+export const getCurrentMonth = () => {
+    return dayjs().month() + 1
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,19 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      flexBasis: {
+        '1/7': '14.2857143%',
+      },
+      aspectRatio: {
+        '2/1': '2 / 1',
       },
     },
   },
